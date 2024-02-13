8fc56a8997abf1c48d958695acea541e6e19bd14
BryteLitty <littyofficials@gmail.com>
2024-02-13 18:49:28 +0000

    test

--- src/pages/yep.jsx
+++ src/pages/yep.jsx
@@ -17,27 +17,32 @@ const Yep = () => {
       <div className="absolute inset-0 bg-black opacity-40"></div>
 
       <div className="flex flex-col items-center w-screen z-10 p-5 text-center">
-        <h1 className="font-bold text-pink-100 text-3xl mb-4">
+        <h1 className="font-black text-white text-3xl mb-4">
           I love you my pretty Queen
         </h1>
 
         <p className="text-yellow-300 text-md">
-          I have a song for you... click this button to listen!
+        In the dance of hearts, you're my sweet tune, <br />
+        Underneath the stars, a love that'll never swoon. <br />
+
+        Happy Valentine's Day, my heart's delight, <br />
+        In your arms, everything feels just right. 
+
         </p>
 
-        <Image
+        {/* <Image
           src={roseImg}
           width={100}
           height={0}
           alt='sksk'
           className='w-200 mb-0'
-        />
+        /> */}
 
         <button
-          className="bg-white text-pink-600 font-bold px-6 py-2 mt-4 rounded-full hover:bg-pink-600 hover:text-white focus:outline-none"
+          className="bg-white text-pink-600 font-black px-6 py-2 mt-4 rounded-full hover:bg-pink-600 hover:text-white focus:outline-none"
           onClick={playSong}
         >
-          {isPlaying ? 'Pause Song' : 'Play Song'}
+          {isPlaying ? 'Pause Song' : 'Unleash my love 💝'}
         </button>
 
         {isPlaying && <LoveSong />}
